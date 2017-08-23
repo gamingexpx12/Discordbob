432f3248921b01543b1fe2ce512b8ffe5bf001bf
gamingexpx12 <gamingexpx12@gmail.com>
2017-08-23 17:40:54 +0200

    Memberdata relationship is now a Relations object, if the bot knows them

--- shared.py
+++ shared.py
@@ -8,8 +8,14 @@ class Relation(Enum):
 class memberdata:
     def __init__(self, name, rela = None, nick = None):
         self.name = name
-        self.relationship = rela
         self.nickname = nick
+        self.relationship = None
+        if rela is not None:
+            for name, member in Relation.__members__.items():
+                print(name, member)
+                if rela == name:
+                    self.relationship = member
+                    break
 
     def __str__(self):
         return "{0}, '{1}'. Prefers {2} ".format(self.name, self.relationship, self.nickname)
